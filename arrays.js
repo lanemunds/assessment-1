ff8b4620e10501b55eece8b9f28b6c2390b24412
lanemunds <lanemunds@gmail.com>
2022-08-19 10:29:55 -0600

    Arrays.jsFinal

--- arrays.js
+++ arrays.js
@@ -3,12 +3,14 @@
 
 const faveColors = ['red', 'green', 'black']
 
-//CODE HERE
+let colorCopy = faveColors.slice(0,3)
+// console.log(colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
+colorCopy.push('purple')
+// console.log(colorCopy)
 
-//CODE HERE
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +20,8 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
-//CODE HERE
+let middleNums = numbers.slice(1, 4)
+// console.log(middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -33,4 +36,10 @@ const numbers = [1, 2, 3, 4, 5]
 
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
-// CODE HERE
+// for(i=0; i< bigOrSmallArray.length; i++){
+//   if(bigOrSmallArray[i] > 100){
+//     console.log('big')
+//   }else if (bigOrSmallArray[i]<= 100){
+//     console.log('small')
+//   }
+// }
