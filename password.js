cd31b2ddb64a9b0648a35191df0215a333aeb8a8
lanemunds <lanemunds@gmail.com>
2022-08-19 11:58:57 -0600

    finalPassword

---
+++ password.js
@@ -0,0 +1,76 @@
+let password = 'thegreenlantern29'
+
+
+if(password.includes ('password'|| 'letmein')=== true){
+    console.log(`
+    >=======>       >>       >=> >=>       >=>     >=> >======>     >=======> 
+    >=>            >>=>      >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=>           >> >=>     >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=====>      >=>  >=>    >=> >=>       >=>     >=> >> >==>      >=====>   
+    >=>         >=====>>=>   >=> >=>       >=>     >=> >=>  >=>     >=>       
+    >=>        >=>      >=>  >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=>       >=>        >=> >=> >=======>   >====>    >=>      >=> >=======> `)
+}else if(password.length >= 10 &&
+    password.length <=20 &&  
+    (password.includes(1) === true|| 
+    password.includes (2) === true|| 
+    password.includes(3) === true|| 
+    password.includes(4) === true|| 
+    password.includes(5) === true|| 
+    password.includes(6) === true|| 
+    password.includes(7) === true|| 
+    password.includes(8) === true|| 
+    password.includes(9) === true|| 
+    password.includes(0) === true) &&
+    (password.includes('a') === true|| 
+    password.includes ('b') === true|| 
+    password.includes('c') === true|| 
+    password.includes('d') === true|| 
+    password.includes('e') === true|| 
+    password.includes('f') === true|| 
+    password.includes('g') === true|| 
+    password.includes('h') === true|| 
+    password.includes('i') === true|| 
+    password.includes('j') === true||
+    password.includes('k') === true||
+    password.includes('l') === true||
+    password.includes('m') === true||
+    password.includes('n') === true||
+    password.includes('o') === true||
+    password.includes('p') === true||
+    password.includes('q') === true||
+    password.includes('r') === true||
+    password.includes('s') === true||
+    password.includes('t') === true||
+    password.includes('u') === true||
+    password.includes('v') === true||
+    password.includes('w') === true||
+    password.includes('x') === true||
+    password.includes('y') === true||
+    password.includes('z') === true 
+    )
+    )
+    
+    
+    {
+    console.log(`
+     _____ __  ______________________________
+    / ___// / / / ____/ ____/ ____/ ___/ ___/
+    \__ \/ / / / /   / /   / __/  \__ \\__ \ 
+   ___/ / /_/ / /___/ /___/ /___ ___/ /__/ / 
+  /____/\____/\____/\____/_____//____/____/  
+                                           
+    
+    `)
+}else(
+    console.log(`
+    >=======>       >>       >=> >=>       >=>     >=> >======>     >=======> 
+    >=>            >>=>      >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=>           >> >=>     >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=====>      >=>  >=>    >=> >=>       >=>     >=> >> >==>      >=====>   
+    >=>         >=====>>=>   >=> >=>       >=>     >=> >=>  >=>     >=>       
+    >=>        >=>      >=>  >=> >=>       >=>     >=> >=>    >=>   >=>       
+    >=>       >=>        >=> >=> >=======>   >====>    >=>      >=> >=======> `))
+   
+
+//    I added in a max character count of 20 and stopped the code from allowing the word password in the password and the phrase "letmein". 
